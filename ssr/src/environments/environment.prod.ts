64a8a206d07d44ed31194b92792b0dcd27d25ffb
navjot <git@netgains.org>
2026-01-01 17:35:50 +0530

    env chnge

--- ssr/src/environments/environment.prod.ts
+++ ssr/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   showBuild: false,
 YOUR_GOOGLE_CLIENT_ID:'824314878142-fb3h8plnidnug1anf52vd8dmeu05cr96.apps.googleusercontent.com',
 LINKEDIN_CLIENT_ID:'86lnkec9zaxz01',
-apiBaseUrl:'http://65.2.122.252/:9000/v1',
+apiBaseUrl:'http://65.2.122.252:9000/v1',
 url:'http://65.2.122.252:4200',
 socketUrl: 'http://65.2.122.252:9000',
   maximumFileSize: 1024,
